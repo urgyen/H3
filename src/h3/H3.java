84245243c756d8ffa5349006463d408fbbe969c0
Urgyen Shakya <urgyenshakya@gmail.com>
2020-05-13 21:57:49 +0545

    a

---
+++ src/h3/H3.java
@@ -0,0 +1,137 @@
+package h3;
+
+public class H3 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello");
+        
+        //Variable 
+        //Datatype
+        
+       // Java ma number lai ki integer or double
+//        15.5
+//                15
+        
+        //VAry huna pauxa
+        
+        // byte bit 
+        int num1= 150; //4 byte
+        int num2= 110;
+       
+        double num3 =  170.5 ; //8 byte
+        double num4 = 22.5;
+       
+        double sum1 = num1 +num2;
+        System.out.println(sum1);
+        
+      // int sum = num1+ num2;
+      //  System.out.println(sum);
+        
+        String firstName= "Puskar";
+        
+        char age = '2'; 
+        
+        int dob = 1994;
+    //    1, 100, 1000
+        //4 byte
+  
+        char ch = 'a';
+        
+        boolean isTrue = false;
+        //1 bit
+        // either 1 or 0
+         
+        //      1           0
+        //      true      false
+        
+        double age1 = 25.5;
+        
+        //  VAriable;
+        //  Datatype;
+           // primitive  --> self independent;
+             //       and non primitive; --> primitive dependent
+        
+        
+//char ?? a
+//        b
+//        d
+//        1
+//        2
+        //Non Primitive??
+        String name= "ASDF";
+       // char A + char S + char  D + char F
+        
+        String schoolName = "ABC";
+        String fathersName = "ASAS";
+        
+        //readabilty
+        // meroNameUrgyenShakyaHo;
+        
+        //camelCasing
+        
+        //Operator;;
+        //operation ;;
+       // 1. Arithmetic Operators
+            //  + , - , / , * , %
+        
+        double num10=11;
+        double num11=5;
+        
+        double remainder = num10 % num11;
+        
+        System.out.println(remainder);
+        
+        //Modulus  ???
+        int number= 25;
+//        number % 2 == 1  --> odd;
+//        number %2 ==0 --> Even
+        
+        // 2.Conditional Opertors??
+         //> , < , <= ,< =  ,
+         
+        int x1 = 15;
+        int x2 = 15;
+        
+        //Assignment
+       // x1 = x2;
+        
+        //Compare
+      //  x1 != x2 --> output true or false
+        
+        
+        //3. Unary Operator
+            //auto increment//auto decrement
+        
+        int apple = 100;
+        //Auto decremnet
+       // apple = apple+1;
+//        apple++; 
+//        //11
+//        apple++;
+//        //12
+//       
+        System.out.println(apple);
+        
+        //4. Logical Operator
+        
+        
+//        &&       ||   
+//        And      Or
+        
+        if((apple >5) || (apple <20) )
+        {
+            System.out.println("Condition Meet");
+        }
+        
+        
+        
+        
+        
+     
+        
+        
+        
+        
+    }   
+}
