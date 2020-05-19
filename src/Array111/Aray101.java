4554167965d56c7196b95472dad7db2bd8975579
Urgyen Shakya <urgyenshakya@gmail.com>
2020-05-19 21:12:55 +0545

    may19

---
+++ src/Array111/Aray101.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Array111;
+
+/**
+ *
+ * @author Urgyen
+ */
+public class Aray101 {
+    
+    public static void main(String[] args){
+        
+        //what is array?
+        //a group of data
+        
+//        //static ;
+//        int age[] = new int[4];
+//        //arrays starts with 0;
+//        
+//        age[0]=20;
+//        age[1]=21;
+//        age[2]=22;
+//        age[3]=23;
+//        
+//       // age[4]=24;
+//        
+////        System.out.println(age[0]);
+////        System.out.println(age[1]);
+////        System.out.println(age[3]);
+//
+//        for(int i=0; i<1000; i++){
+//            System.out.println(age[i]);
+//        }
+        
+        //String nameList[]={"Urgyen","Apple","Banana", "Cat", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju", "Abishek" , "Puskar" , "Sanju"};
+        
+//        System.out.println(nameList.length);
+//        
+//        for(int i=0; i < nameList.length ;i++){
+//            System.out.println(nameList[i]);
+//        }
+        
+//        array? data type??
+//                String int;
+        
+        
+        //String nameList[]={"A", "B" , "C", "D" , "E"};
+        
+        //for each loop
+        // A---> a1 --> Print A;
+        // B --> a1 --> print B;
+        // C --> a1 --> Print c;
+         // D --> a1 --> Print d;
+         // E --> a1 --> Print e;
+        
+        
+        //String a1= "A";
+        
+//        for( String a1 : nameList){
+//            System.out.println(a1);
+//        }
+        
+       // Funtion ma ARRay ; 
+        
+        
+    }
+    
+    
+}
